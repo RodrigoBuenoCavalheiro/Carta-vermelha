4813fcb6671812a07281292053d2ce7c66f78811
Rodrigo Bueno Cavalheiro <rodrigo.bueno.cavalheiro@escola.pr.gov.br>
2024-09-08 20:01:11 -0300

    adcionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,69 @@
+Skip to Play Sketch
+Cookies
+The p5.js Editor uses cookies. Some are essential to the website functionality and allow you to manage an account and preferences. Others are not essential—they are used for analytics and allow us to learn more about our community. We never sell this data or use it for advertising. You can decide which cookies you would like to allow, and learn more in our Privacy Policy.
+
+
+Allow All
+
+Allow Essential
+
+Retangulo
+by rodrigo.bueno.cavalheiro
+
+rodrigo.bueno.cavalheiro
+My Stuff
+Settings
+Log Out
+
+Arquivo
+Novo
+Salvar
+Exemplos
+Editar
+Organizar Código
+Procurar
+Esboço
+Adicionar Arquivo
+Adicionar Pasta
+Settings
+Preferences
+Language
+Ajuda
+Atalhos de Teclado
+Referência
+Sobre
+
+
+sketch.js
+1
+function setup() {
+2
+  background("white")
+3
+  createCanvas(400, 400);
+4
+}
+5
+​
+6
+function draw() {
+7
+  stroke("blue");
+8
+  fill("red");
+9
+  random()
+10
+  
+11
+  if (mouseIsPressed) {
+12
+    rect(mouseX, mouseY, 20, 35);
+13
+  }   
+14
+}
+15
+​
+Não há mensagens de Lint
+Linha atuallinha 16
